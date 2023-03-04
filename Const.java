3a130d1e936ee4be7418c4e359afc282f555a373
TaoEvan <Tao.Evan.Pi@gmail.com>
2023-03-04 01:58:01 -0500

    duckng title screen

--- Const.java
+++ Const.java
@@ -3,6 +3,6 @@ import java.awt.Toolkit;
 
 public class Const {
     public static final Dimension SCREENSIZE = Toolkit.getDefaultToolkit().getScreenSize();
-    public static final int WIDTH = 1500;
-    public static final int HEIGHT = 820;
+    public static final int WIDTH = 1920;
+    public static final int HEIGHT = 1080;
 }
--- TitleMenu.java
+++ TitleMenu.java
@@ -1,13 +1,46 @@
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JLabel;
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 public class TitleMenu extends JFrame{
-    public TitleMenu(){
+    BufferedImage background;
+    public void initButtons(){
+        ImageIcon startIcon = new ImageIcon("./Assets/Icons/folder.png");
+        JButton startButton = new JButton(startIcon);
+        startButton.setBorderPainted(false);
+        startButton.setFocusPainted(false);
+        startButton.setContentAreaFilled(false);
+        this.add(startButton);
+        startButton.setLocation(500,500);
+    }
+    public TitleMenu() {
         GraphicsEnvironment graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice device = graphics.getDefaultScreenDevice();
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         device.setFullScreenWindow(this);
+        try{
+            background = ImageIO.read(getClass().getResourceAsStream("Assets/General/Background.png"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        this.setLayout(null);
+        initButtons();
         this.setVisible(true);
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponents(g);
+        Graphics2D g2d = (Graphics2D)g;
+
+        setBackground(Color.BLACK);
+
+        g2d.drawImage(background, 0, 0, (int)Const.WIDTH, (int)Const.HEIGHT, null);
     }
 }
